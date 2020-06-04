3bd1fa0750f15a09ffaa2f1219b3d9c9c6ff6b24
Michael Daly <mikedaly81@protonmail.com>
2020-06-03 21:16:13 -0400

    lab 2 finished

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -1,14 +1,90 @@
 package com.example.androidlabs;
 
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
+import android.widget.Switch;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Switch sw;
+    private CheckBox check;
+    private Button button;
+    private Snackbar snack;
+    private static boolean locale = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_grid);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(v -> Toast.makeText(this, getResources().getString(R.string.toast_message), Toast.LENGTH_LONG).show());
+
+        check = findViewById(R.id.checkBox);
+        check.setOnCheckedChangeListener((view, isChecked) -> snackbarDisplay());
+
+        sw = findViewById(R.id.switch1);
+        sw.setOnCheckedChangeListener((view, isChecked) -> updateSnackbarText());
+
+        snack = makeSnackbar();
+
+        ImageButton flag = findViewById(R.id.imageView);
+        flag.setOnClickListener((v -> setLocale()));
+
     }
+
+    private String widgetStatus(boolean bool) {
+        return (bool) ? getResources().getString(R.string.status_on) : getResources().getString(R.string.status_off);
+    }
+
+    private void updateSnackbarText() {
+        snack.setText(getResources().getString(R.string.snackbar_message) + widgetStatus(sw.isChecked()));
+        snack.show();
+    }
+
+    private void snackbarDisplay() {
+        if (check.isChecked()) snack.show();
+        else snack.dismiss();
+    }
+
+    private Snackbar makeSnackbar() {
+        return Snackbar.make(button, getResources().getString(R.string.snackbar_message) + widgetStatus(sw.isChecked()), Snackbar.LENGTH_INDEFINITE).setAction(getResources().getString(R.string.button_undo), (view -> {
+            sw.toggle();
+            snack = makeSnackbar();
+            snack.show();
+        }));
+    }
+
+    private void setLocale() {
+        Locale myLocale;
+        if (locale) {
+            myLocale = new Locale("fr");
+        } else {
+            myLocale = new Locale("en");
+        }
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        conf.locale = myLocale;
+        res.updateConfiguration(conf, dm);
+        Intent refresh = new Intent(this, MainActivity.class);
+        finish();
+        startActivity(refresh);
+        locale = !locale;
+    }
+
 }
+
